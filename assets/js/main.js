c29956297c6636b09def084343d3e3bef6132af7
gursewak <sohi.gursewak@gmail.com>
2025-07-24 12:42:10 +0530

    homepage

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+(function() {
+
+  // JS Loaded
+  let body = document.body;
+  body.classList.add('js-loaded');
+ 
+  // AOS Init
+  AOS.init({
+    duration: 800, 
+    once: true,   
+    offset: 100,
+  });
+
+  // Hero Swiper
+  document.querySelectorAll(".hero-swiper").forEach((container) => {
+      const swiperEl = container.querySelector(".swiper");
+  
+      new Swiper(swiperEl, {
+          slidesPerView: 1,
+          speed: 800,
+          autoplay: {
+            delay: 5000,
+          },
+          effect: "fade",
+          spaceBetween: 0,
+          grabCursor: true,
+          loop: true
+      });
+  });
+ 
+
+
+})();
+ 
