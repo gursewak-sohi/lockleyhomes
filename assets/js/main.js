e3f0c254ed2772e9e1590eead10f870b5721c96b
gursewak <sohi.gursewak@gmail.com>
2025-07-25 22:34:23 +0530

    little acre

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,29 @@
       });
   });
 
+
+  // Hero Swiper
+  document.querySelectorAll(".property-details-swiper").forEach((container) => {
+    const swiperEl = container.querySelector(".swiper"),
+          nextEl = container.querySelector(".swiper-button-next"),
+          prevEl = container.querySelector(".swiper-button-prev");
+
+
+      new Swiper(swiperEl, {
+          slidesPerView: 1,
+          speed: 800,
+          autoplay: {
+            delay: 5000,
+          },
+          spaceBetween: 0,
+          grabCursor: true,
+          loop: true,
+          navigation: {
+            nextEl: nextEl,
+            prevEl: prevEl,
+          },
+      });
+  });
   
 
 
